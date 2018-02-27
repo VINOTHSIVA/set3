ef8a0032190bc6e9d3eb0aff95f28fdacf2a104d
VINOTHSIVA <35728684+VINOTHSIVA@users.noreply.github.com>
2018-02-27 16:43:36 +0530

    Rename arithmeticprogression.c to Arithmeticprogression.c

---
+++ Arithmeticprogression.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,x,y,i,sum=0,l;
+    printf("enter the no of terms:");
+    scanf("%d",&n);
+    printf("enter the starting num and diff:");
+    scanf("%d%d",&x,&y);
+    for(i=0;i<n;i++)
+    {
+            l=(n*y)+x-1;
+            sum=((x+l)*n)/2;
+    }
+    printf("\nsum of AP series:%d",sum);
+}
