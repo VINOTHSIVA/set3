46949614e69762d53c9e93dafba5d410776e1f37
VINOTHSIVA <35728684+VINOTHSIVA@users.noreply.github.com>
2018-03-13 16:35:15 +0530

    Create hourminsub.c

---
+++ hourminsub.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int min1,hour1,min2,hour2,min,hour,total;
+printf("enter the hour and min");
+scanf("%d%d",&hour1,&min1);
+printf("enter the secont hour and min");
+scanf("%d%d",&hour2,&min2);
+total=((hour1*60)+min1)-((hour2*60)+min2);
+hour=total/60;
+min=(total%60);
+printf("%d\t%d",hour,min);
+}
+
