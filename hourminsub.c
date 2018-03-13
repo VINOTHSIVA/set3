a01c4ac22378c235a9f5148977230c8bfa178cf0
VINOTHSIVA <35728684+VINOTHSIVA@users.noreply.github.com>
2018-03-13 16:42:40 +0530

    Update hourminsub.c

--- hourminsub.c
+++ hourminsub.c
@@ -6,7 +6,10 @@ printf("enter the hour and min");
 scanf("%d%d",&hour1,&min1);
 printf("enter the secont hour and min");
 scanf("%d%d",&hour2,&min2);
+if(((hour1*60)+min1)>((hour2*60)+min2))
 total=((hour1*60)+min1)-((hour2*60)+min2);
+else
+total=((hour2*60)+min2)-((hour1*60)+min1);
 hour=total/60;
 min=(total%60);
 printf("%d\t%d",hour,min);
